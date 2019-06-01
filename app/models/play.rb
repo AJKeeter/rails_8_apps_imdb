25a5b175ed1b9a550bc61c35db75f8a7c35fee5d
Alec Keeter <aleckeeter94@gmail.com>
2019-05-31 21:29:27 -0500

    adds images to index page

--- app/models/play.rb
+++ app/models/play.rb
@@ -1,7 +1,9 @@
 class Play < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :reviews
 
-  has_attatched_file :play_img, :styles => { :play_index => "250x350>", play_show => "325x475>" }, :default_url => "/images/:style/missing.png"
-  validates_attatchment_content_type :play_img, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :play_img, :styles => { :play_index => "1250x1350>", :play_show => "1000x1005>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, :content_type => /\Aimage\/.*\Z/
 end
+
