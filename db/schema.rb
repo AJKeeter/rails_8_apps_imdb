c5bd58f950a26e92b0a6cd0af7da2878729a976d
Alec Keeter <aleckeeter94@gmail.com>
2019-06-01 19:00:26 -0500

    Adds Schema migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190601171900) do
+ActiveRecord::Schema.define(version: 20190601235206) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20190601171900) do
     t.text     "comment",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "play_id",    limit: 4
   end
 
   create_table "users", force: :cascade do |t|
